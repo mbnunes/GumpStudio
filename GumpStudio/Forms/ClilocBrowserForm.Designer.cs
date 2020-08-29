4f2d2a73d9661e4e449f231906a40c60c0602402
deccer <decccer@outlook.com>
2020-08-29 22:52:35 +0400

    First set of changes. Redo ClilocBrowser using a virtual Listview rather than a hand drawn Listbox.

---
+++ GumpStudio/Forms/ClilocBrowserForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace GumpStudio.Forms
+{
+    partial class ClilocBrowserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.LanguageComboBox = new System.Windows.Forms.ComboBox();
+            this.LanguageLabel = new System.Windows.Forms.Label();
+            this.EntriesListView = new System.Windows.Forms.ListView();
+            this.IdHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ValueHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TableLayoutPanel1
+            // 
+            this.TableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.TableLayoutPanel1.ColumnCount = 2;
+            this.TableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TableLayoutPanel1.Controls.Add(this.OkButton, 0, 0);
+            this.TableLayoutPanel1.Controls.Add(this.CancelButton, 1, 0);
+            this.TableLayoutPanel1.Location = new System.Drawing.Point(526, 452);
+            this.TableLayoutPanel1.Name = "TableLayoutPanel1";
+            this.TableLayoutPanel1.RowCount = 1;
+            this.TableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TableLayoutPanel1.Size = new System.Drawing.Size(170, 33);
+            this.TableLayoutPanel1.TabIndex = 4;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OkButton.Location = new System.Drawing.Point(3, 3);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(78, 27);
+            this.OkButton.TabIndex = 0;
+            this.OkButton.Text = global::GumpStudio.Properties.Resources.OK;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Location = new System.Drawing.Point(88, 3);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(78, 27);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = global::GumpStudio.Properties.Resources.Cancel;
+            // 
+            // LanguageComboBox
+            // 
+            this.LanguageComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.LanguageComboBox.FormattingEnabled = true;
+            this.LanguageComboBox.Location = new System.Drawing.Point(85, 458);
+            this.LanguageComboBox.Name = "LanguageComboBox";
+            this.LanguageComboBox.Size = new System.Drawing.Size(140, 23);
+            this.LanguageComboBox.TabIndex = 7;
+            this.LanguageComboBox.SelectedIndexChanged += new System.EventHandler(this.cboLanguage_SelectedIndexChanged);
+            // 
+            // LanguageLabel
+            // 
+            this.LanguageLabel.AutoSize = true;
+            this.LanguageLabel.Location = new System.Drawing.Point(14, 462);
+            this.LanguageLabel.Name = "LanguageLabel";
+            this.LanguageLabel.Size = new System.Drawing.Size(59, 15);
+            this.LanguageLabel.TabIndex = 6;
+            this.LanguageLabel.Text = "Language";
+            // 
+            // EntriesListView
+            // 
+            this.EntriesListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.EntriesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.IdHeader,
+            this.ValueHeader});
+            this.EntriesListView.FullRowSelect = true;
+            this.EntriesListView.HideSelection = false;
+            this.EntriesListView.Location = new System.Drawing.Point(14, 14);
+            this.EntriesListView.Name = "EntriesListView";
+            this.EntriesListView.Size = new System.Drawing.Size(682, 431);
+            this.EntriesListView.TabIndex = 8;
+            this.EntriesListView.UseCompatibleStateImageBehavior = false;
+            this.EntriesListView.View = System.Windows.Forms.View.Details;
+            this.EntriesListView.VirtualListSize = 100000;
+            this.EntriesListView.VirtualMode = true;
+            this.EntriesListView.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.lvEntries_RetrieveVirtualItem);
+            this.EntriesListView.SelectedIndexChanged += new System.EventHandler(this.lvEntries_SelectedIndexChanged);
+            // 
+            // IdHeader
+            // 
+            this.IdHeader.Text = "Id";
+            this.IdHeader.Width = 86;
+            // 
+            // ValueHeader
+            // 
+            this.ValueHeader.Text = "Value";
+            this.ValueHeader.Width = 486;
+            // 
+            // ClilocBrowserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 500);
+            this.Controls.Add(this.EntriesListView);
+            this.Controls.Add(this.TableLayoutPanel1);
+            this.Controls.Add(this.LanguageComboBox);
+            this.Controls.Add(this.LanguageLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "ClilocBrowserForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client Localization";
+            this.Load += new System.EventHandler(this.ClilocBrowserForm_Load);
+            this.TableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TableLayoutPanel1;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.ComboBox LanguageComboBox;
+        private System.Windows.Forms.Label LanguageLabel;
+        private System.Windows.Forms.ListView EntriesListView;
+        private System.Windows.Forms.ColumnHeader IdHeader;
+        private System.Windows.Forms.ColumnHeader ValueHeader;
+    }
+}
